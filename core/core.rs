0c83cb0bcec0a05c923551710a97d21a884f25ee
Maciej-Grzesik <mackeplz61@gmail.com>
2025-03-21 13:01:20 +0100

    wip, transferring work to pc

--- core/core.rs
+++ core/core.rs
@@ -1,15 +1,13 @@
-struct Instance {
+pub struct Instance {
     nodes: Vec<Node>,
     capacity: i32,
     depo_id: i32,   
 }
 
-struct Node {
+pub struct Node {
+    id: i32,
     x: i32,
     y: i32,
     demand: i32,
 }
 
-pub fn test123() {
-    println!("asd");
-}
--- src/instance_loader.rs
+++ src/instance_loader.rs
@@ -1,9 +1,8 @@
-#[path ="../core/core.rs"]mod core;
 use std::fs;
 use std::str;
 
-pub fn load_instance(fp: &str) {
-   //let content = fs::read_to_string(fp).expect("");
-   core::test123();
-   //println!("{content}");
-}       
+
+pub fn load_instance(fp: &str) -> io::Result<Instance>{
+   let content = fs::read_to_string(fp).expect("");
+   println!("{content}");
+}    
